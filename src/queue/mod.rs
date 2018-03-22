1460386ca05c5a52d079526cabdd9dcd21fadba0
Oleksii Lystopadov <oleksiily@softwareplanetgroup.com>
2018-03-22 15:24:26 +0200

    Add push message method for Queue

--- src/queue/mod.rs
+++ src/queue/mod.rs
@@ -1,24 +1,70 @@
 use super::*;
 
+use serde_json::{Error, Value};
+
 pub struct Queue<'a> {
     pub client: &'a mut Client,
-    pub name: String
+    pub name: String,
 }
 
 impl<'a> Queue<'a> {
-     pub fn info(&mut self) -> String {
-        let path = format!("{}queues/{}",self.client.base_path, self.name);
+    pub fn info(&mut self) -> String {
+        let path = format!("{}queues/{}", self.client.base_path, self.name);
         let mut req = Request::new(Method::Get, path.parse().unwrap());
         req.headers_mut().set(ContentType::json());
 
-        let authorization_header = format!("OAuth {}",self.client.token);
+        let authorization_header = format!("OAuth {}", self.client.token);
         req.headers_mut().set(Authorization(authorization_header));
 
-        let get = self.client.http_client.client.request(req).and_then(|res| {
-            res.body().concat2()
-        });
-        let res = self.client.http_client.core.run(get).unwrap();
+        let get = self.client
+            .http_client
+            .client
+            .request(req)
+            .and_then(|res| res.body().concat2());
+        
+        let res = self.client
+            .http_client
+            .core
+            .run(get)
+            .unwrap();
 
         String::from_utf8(res.to_vec()).unwrap()
+    }
+
+    pub fn push_message(&mut self, message: &str) -> String {
+        let path = format!("{}queues/{}/messages", self.client.base_path, self.name);
+        let mut req = Request::new(Method::Post, path.parse().unwrap());
+        req.headers_mut().set(ContentType::json());
+
+        let authorization_header = format!("OAuth {}", self.client.token);
+        req.headers_mut().set(Authorization(authorization_header));
+
+        let message = json!(
+            {
+                "messages": [
+                    {
+                        "body": message
+                    }
+                ]
+            }
+        );
+
+        req.set_body(message.to_string());
+
+        let post = self.client
+            .http_client
+            .client
+            .request(req)
+            .and_then(|res| res.body().concat2());
+        
+        let res = self.client
+            .http_client
+            .core
+            .run(post)
+            .unwrap();
+
+        let response_body = String::from_utf8(res.to_vec()).unwrap();
+        let response: Value = serde_json::from_str(&response_body).unwrap();
+        response["ids"][0].to_string()
     }
 }
--- tests/client.rs
+++ tests/client.rs
@@ -11,7 +11,16 @@ mod tests {
         let mq = Client::from_env();
     }
 
-    #[test]
+    #[test] 
+    fn get_queue() {
+        let mut mq = Client::from_env();
+        let queue_name = String::from("test-pull");
+        let q = mq.queue(queue_name.clone());
+
+        assert_eq!(q.name, queue_name);
+    }
+
+    #[test] 
     fn get_queue_info() {
         let mut mq = Client::from_env();
         let queue_name = String::from("test-pull");
@@ -21,6 +30,16 @@ mod tests {
         assert!(queue_info.len() > 0);
         assert!(!queue_info.contains("Queue not found"));
         assert!(!queue_info.contains("Invalid project/token combination"));
+    }
+
+    #[test]
+    fn push_message() {
+        let mut mq = Client::from_env();
+        let queue_name = String::from("test-pull");
+        let mut q = mq.queue(queue_name.clone());
+        let id = q.push_message("test message");
+
+        assert!(id.len() > 0);
     }
 
 }
